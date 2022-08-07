daaa7b599669566852a414bb13fb833c55852a86
DESKTOP-GEFTSHN\ADMIN <tqtteams1st@gmail.com>
2022-08-07 22:58:21 +0700

    commit

---
+++ js.js
@@ -0,0 +1,73 @@
+
+
+var listParamObj = [];
+
+function mappingParam() {
+  var quey = document.getElementById("queyinput").value
+  var param = document.getElementById("paramInput").value
+  console.log(quey);
+  console.log(param);
+
+
+
+    var listParam = param.split(',');
+for (let i = 0; i < listParam.length; i++) {
+    listParam[i].trim();
+    var arr = listParam[i].split("(");
+    var vl = arr[0];
+    var type = arr[1];
+    type = type.substring(0, type.length - 1);
+
+    const paramObj = {
+        vl: vl,
+        type: type,
+        index: i
+    };
+
+    listParamObj.push(paramObj);
+}
+
+var indices = [];
+var count = 0;
+for(var i=0; i<quey.length;i++) {
+    if (quey[i] === "?"){
+        quey = setCharAt(quey,i,getPramByIndex(count));
+        count++;
+    }
+}
+    $.ajax({
+        url: 'http://sqlformat.org/api/v1/format',
+        type: 'POST',
+        dataType: 'json',
+        crossDomain: true,
+        data: {sql: quey, reindent: 1},
+        success: (data)=>{
+              document.getElementById('result').value = data.result;
+        },
+    });
+
+}
+
+function getPramByIndex(indexs) {
+    for (let index = 0; index < listParamObj.length; index++) {
+        const element = listParamObj[index];
+        if(element.index == indexs){
+            var vl;
+            switch (element.type) {
+               case "String":
+                   vl = '"'+element.vl.trim()+'"'
+                   break;
+               default:
+                   vl = element.vl;
+            }
+            return vl;
+          }
+    }
+}
+
+
+function setCharAt(str,index,chr) {
+    if(index > str.length-1) return str;
+    return str.substring(0,index) + chr + str.substring(index+1);
+}
+
