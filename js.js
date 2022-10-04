6d210e2873c326ffd01cc1bd528464bd775a7edf
DESKTOP-GEFTSHN\ADMIN <tqtteams1st@gmail.com>
2022-10-04 21:49:18 +0700

    update

--- js.js
+++ js.js
@@ -1,56 +1,67 @@
 
-
-var listParamObj = [];
-
 function mappingParam() {
-  var quey = document.getElementById("queyinput").value
-  var param = document.getElementById("paramInput").value
-  console.log(quey);
-  console.log(param);
-
-
-
+var listParamObj = [];
+    var quey = document.getElementById("queyinput").value
+    var param = document.getElementById("paramInput").value
     var listParam = param.split(',');
-for (let i = 0; i < listParam.length; i++) {
-    listParam[i].trim();
-    var arr = listParam[i].split("(");
-    var vl = arr[0];
-    var type = arr[1];
-    console.log(type);
-    if(type !== "" && type !== undefined && type !== null){
-        type = type.substring(0, type.length - 1);
-    } else {
-        type = null;
+    for (let i = 0; i < listParam.length; i++) {
+        listParam[i].trim();
+        var arr = listParam[i].split("(");
+        var vl = arr[0];
+        var type = arr[1];
+        if (type !== "" && type !== undefined && type !== null) {
+            type = type.substring(0, type.length - 1);
+        } else {
+            $("#loadingModal").modal('hide');
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Input valid, try again or view tutorial',
+            })
+            $("#loadingModal").modal('hide');
+            return
+        }
+        const paramObj = {
+            vl: vl,
+            type: type,
+            index: i
+        };
+        listParamObj.push(paramObj);
+    }
+    var count = 0;
+    for (var i = 0; i < quey.length; i++) {
+        if (quey[i] === "?") {
+            quey = setCharAt(quey, i, getPramByIndex(count, listParamObj));
+            count++;
+        }
     }
 
-    const paramObj = {
-        vl: vl,
-        type: type,
-        index: i
-    };
-
-    listParamObj.push(paramObj);
-}
-
-var indices = [];
-var count = 0;
-for(var i=0; i<quey.length;i++) {
-    if (quey[i] === "?"){
-        quey = setCharAt(quey,i,getPramByIndex(count));
-        count++;
+    console.log(listParamObj.length);
+    console.log(count);
+    if (listParamObj.length !== count) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Input valid, try again or view tutorial',
+        })
+        return
     }
-}
+
+    $("#loadingModal").modal('show');
     $.ajax({
         url: 'https://sqlformat.org/api/v1/format',
         type: 'POST',
         dataType: 'json',
         crossDomain: true,
-        data: {sql: quey, reindent: 1},
-        success: (data)=>{
-              document.getElementById('result').value = data.result;
-                $("#loadingModal").modal('hide');
+        data: {
+            sql: quey,
+            reindent: 1
         },
-        done: ()=>{
+        success: (data) => {
+            document.getElementById('result').value = data.result;
+            $("#loadingModal").modal('hide');
+        },
+        done: () => {
             $("#loadingModal").modal('hide');
 
         }
@@ -58,35 +69,35 @@ for(var i=0; i<quey.length;i++) {
 
 }
 
-function getPramByIndex(indexs) {
+function getPramByIndex(indexs,listParamObj) {
     for (let index = 0; index < listParamObj.length; index++) {
         const element = listParamObj[index];
-        if(element.index == indexs){
+        if (element.index == indexs) {
             var vl;
-            if(element.type !== null){
+            if (element.type !== null) {
                 switch (element.type.toLowerCase()) {
                     case "string":
-                        vl = '"'+element.vl.trim()+'"'
+                        vl = '"' + element.vl.trim() + '"'
                         break;
-                        case "timestamp":
-                        vl = '"'+element.vl.trim()+'"'
+                    case "timestamp":
+                        vl = '"' + element.vl.trim() + '"'
                         break;
                     default:
                         vl = element.vl;
-                 }
+                }
             } else {
                 vl = element.vl;
             }
-            
+
         }
     }
     return vl;
 }
 
 
-function setCharAt(str,index,chr) {
-    if(index > str.length-1) return str;
-    return str.substring(0,index) + chr + str.substring(index+1);
+function setCharAt(str, index, chr) {
+    if (index > str.length - 1) return str;
+    return str.substring(0, index) + chr + str.substring(index + 1);
 }
 
 function copy() {
@@ -97,17 +108,15 @@ function copy() {
     sampleTextarea.select(); //select textarea contenrs
     document.execCommand("copy");
     document.body.removeChild(sampleTextarea);
-  }
-  
+}
 
-  async  function  pasteQuery(){
+
+async function pasteQuery() {
     const text = await navigator.clipboard.readText();
     $("#queyinput").val(text);
-  }
+}
 
-  async  function  pasteParm(){
+async function pasteParm() {
     const text = await navigator.clipboard.readText();
     $("#paramInput").val(text);
-  }
-
-  
+}
